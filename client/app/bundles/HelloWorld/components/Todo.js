68c34b39aecdb3dde8af6bfbd6e2d89caff600d2
Iggy <igoririanto@rocketmail.com>
2017-02-17 19:50:42 -0800

    add currentTodo and todos object

--- client/app/bundles/HelloWorld/components/Todo.js
+++ client/app/bundles/HelloWorld/components/Todo.js
@@ -4,16 +4,27 @@ class Todo extends React.Component{
   constructor(){
     super();
     this.state={
-      todos: 'hello'
+      todos: [
+        {id: 1, name:'Learn react_on_rails', isComplete: false},
+        {id: 2, name:'build some app', isComplete: false},
+        {id: 3, name:'Ship it!', isComplete: false}
+      ], currentTodo: 'current todo!'
     }
   }
+  todoHandler(e){
+    this.setState({
+      currentTodo: e.target.value
+    })
+  };
   render(){
     return (
       <div>
         <h2>Hello from todo!</h2>
-        <input type="text" />
-        
-        {this.state.todos}
+        <input type="text" onChange={this.todoHandler.bind(this)}/>
+        Current todo:{this.state.currentTodo}
+        <ul>
+          {this.state.todos.map(todo => <li key={todo.id}>{todo.name}</li>)}
+        </ul>
       </div>
     )
   }
